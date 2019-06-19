4c6a06743f3eef0e796bee4ac9cd8419f52957ec
Nurlyssultan <nurlyssultan.sabitov@gmail.com>
2019-06-20 01:32:58 +0600

    Added all login and registration forms and views

--- UserAuthApp/basicUserAuthApp/urls.py
+++ UserAuthApp/basicUserAuthApp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from basicUserAuthApp import views
 app_name = 'basicUserAuthApp'
 urlpatterns = [
-    path('login/',views.login, name='login'),
+    path('login/',views.login_page, name='login_page'),
     path('registration/',views.registration, name='registration'),
     path('log_in/',views.log_in, name='log_in'),
     path('register/',views.register, name='register'),
--- UserAuthApp/basicUserAuthApp/views.py
+++ UserAuthApp/basicUserAuthApp/views.py
@@ -3,11 +3,11 @@ from django.urls import reverse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect, HttpResponse
-from basicUserAuthApp import models
+from basicUserAuthApp.models import User
 # Create your views here.
 def index(request):
     return render(request,'basicUserAuthApp/index.html')
-def login(request):
+def login_page(request):
     return render(request,'basicUserAuthApp/login.html')
 def registration(request):
     return render(request,'basicUserAuthApp/registration.html')
@@ -15,8 +15,23 @@ def log_in(request):
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
+        print(f"Input Username:{username}")
+        user = authenticate(username = username,password = password)
+        if user:
+            print("User is authenticated.")
+            if user.is_active:
+                print("User is active.")
+                login(request,user)
+                return HttpResponseRedirect(reverse('index'))
+            else:
+                return HttpResponse(f"{username} account is not active")
+        else:
+            print("Someone tried to login, but fortunantly failed!")
+            print(f"Username: {username}")
+            return HttpResponse("Login is failed!")
     print(username)
     return index(request)
+
 def register(request):
     if request.method == 'POST':
         first_name = request.POST.get('first_name')
@@ -25,8 +40,19 @@ def register(request):
         email = request.POST.get('email')
         password = request.POST.get('password')
         reset_password = request.POST.get('rest_password')
-    print(first_name)
-    print(last_name)
-    print(username)
-    print(email)
+        if password == reset_password:
+            user = User.objects.create_user(username,email,password)
+            user.first_name = first_name
+            user.last_name = last_name
+            user.save()
+            if user :
+                print("User is registered!")
+                print(f"User:{user}")
+        else:
+            return HttpResponse("Password typed differently!")
+    print(f"Input First_name:{first_name}")
+    print(f"Input Last_name:{last_name}")
+    print(f"Input Username:{username}")
+    print(f"Input Email:{email}")
+    print(f"Status password{password == reset_password}")
     return index(request)
